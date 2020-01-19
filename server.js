c4a54e858342108ec057767f81e8856af442a2c8
Harrisonkamau <kamauharrison87@gmail.com>
2020-01-19 15:20:27 +0300

    Successfully retrieved Medium user & all their publications

--- server.js
+++ server.js
@@ -1,8 +1,21 @@
+require('dotenv').config();
+
+const config = require('config');
 const http = require('http');
+
+const PORT = config.get('port');
+
+const { getMediumUser, getPosts } = require('./lib');
 
 const server = http.createServer((req, res) => {
   res.writeHead(200);
   res.end('hello world');
 });
 
-server.listen(4000, () => console.log('Server running on 4000'));
+
+server.listen(PORT, async () => {
+    console.log(`Server running on ${PORT}`);
+    await getMediumUser();
+    await getPosts();
+  }
+);
