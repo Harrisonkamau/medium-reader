c8393c8acda906b3ade6285bdec9624aa7db3b14
Harrisonkamau <kamauharrison87@gmail.com>
2020-01-19 16:01:26 +0300

    No need to call getMediumUser twice

--- server.js
+++ server.js
@@ -5,17 +5,15 @@ const http = require('http');
 
 const PORT = config.get('port');
 
-const { getMediumUser, getPosts } = require('./lib');
+const { getPosts } = require('./lib');
 
 const server = http.createServer((req, res) => {
   res.writeHead(200);
   res.end('hello world');
 });
 
-
 server.listen(PORT, async () => {
     console.log(`Server running on ${PORT}`);
-    await getMediumUser();
     await getPosts();
   }
 );
